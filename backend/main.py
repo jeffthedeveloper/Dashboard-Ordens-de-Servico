e2d9294a5324e746ecd72a754c964c8ad8b60b04
jeffthedeveloper <jeffersonfir@gmail.com>
2025-09-04 19:33:46 -0300

    updating the requirements.txt file with the render.yaml directions of IAC (infrastructure patters)

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,17 @@
 from app import create_app
 from flask import send_from_directory
+import os  # Importe o 'os' para a função serve funcionar
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-# configura o Flask para servir os arquivos estáticos da pasta 'static'
+# Configura o Flask para servir os arquivos estáticos da pasta 'static'
+# Esta rota é para a abordagem de servir tudo pelo Flask, se optar por ela.
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
